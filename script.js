50f1b20f1a293a6cfd219de01cdee11275e692ee
Thiago Antonello Vargas <thiantonello@gmail.com>
2023-02-06 19:34:23 -0300

    feat: add new game function and hide dice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-'use strict';
+"use strict";
+
+// Selecting elements
+const scoreElement0 = document.querySelector("#score--0");
+const scoreElement1 = document.getElementById("score--1");
+const diceElement = document.querySelector(".dice");
+
+function newGame() {
+  scoreElement0.textContent = 0;
+  scoreElement1.textContent = 0;
+
+  diceElement.classList.add("hidden");
+}
+
+newGame();
