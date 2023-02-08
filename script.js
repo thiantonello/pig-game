221f16c78d4f73fa62411791888304d2cf5f0254
Thiago Antonello Vargas <thiantonello@gmail.com>
2023-02-07 23:37:55 -0300

    feat: add new game and roll dice functionalities
    add new game function on btn--new click
    add roll dice function on btn--roll click
      shows the die image according to the rolled number

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 const scoreElement0 = document.querySelector("#score--0");
 const scoreElement1 = document.getElementById("score--1");
 const diceElement = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
 function newGame() {
   scoreElement0.textContent = 0;
@@ -13,3 +16,19 @@ function newGame() {
 }
 
 newGame();
+
+btnNew.addEventListener("click", newGame);
+
+btnRoll.addEventListener("click", function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  console.log(dice);
+
+  diceElement.classList.remove("hidden");
+
+  diceElement.setAttribute("src", changeDiceImage());
+
+  function changeDiceImage() {
+    return `./src/img/dice-${dice}.png`;
+  }
+});
