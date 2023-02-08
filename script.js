440dcf43a5c54a638b10d8e89198cabb34d30354
Thiago Antonello Vargas <thiantonello@gmail.com>
2023-02-08 17:36:36 -0300

    feat: add players current scores and player swap

--- script.js
+++ script.js
@@ -3,14 +3,29 @@
 // Selecting elements
 const scoreElement0 = document.querySelector("#score--0");
 const scoreElement1 = document.getElementById("score--1");
+
+const currentElement0 = document.getElementById("current--0");
+const currentElement1 = document.getElementById("current--1");
+
 const diceElement = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
+let currentScore = 0;
+let activePlayer = 0;
+
+const scores = [0, 0];
+
 function newGame() {
   scoreElement0.textContent = 0;
   scoreElement1.textContent = 0;
+
+  scores[0] = 0;
+  scores[1] = 0;
+
+  currentScore = 0;
+  activePlayer = 0;
 
   diceElement.classList.add("hidden");
 }
@@ -31,5 +46,19 @@ btnRoll.addEventListener("click", function () {
 
   function changeDiceImage() {
     return `./src/img/dice-${dice}.png`;
+  }
+
+  // Check for rolled 1
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    // Add currentScore to activePlayer currentScore
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // Switch to the next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
   }
 });
